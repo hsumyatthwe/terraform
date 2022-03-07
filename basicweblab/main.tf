75ffa0a34eff55d9864d910073c4e4aa19f33013
hsumyatthwe <hsumyatthwe@gtmh-telecom.com>
2022-03-07 15:24:34 +0630

    comit error update

--- basicweblab/main.tf
+++ basicweblab/main.tf
@@ -7,19 +7,20 @@ resource "aws_instance" "web" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.web.id]
     user_data = <<EOF 
-    #!/bin/bash
-    yum -y update &&
-    yum -y install httpd
-    MYIP=`curl http://169.254.169.254/lastest/meta-data/local-ipv4`
-    echo "<h2>Webserver with PrivateIP: $MYIP</h2><br>Build by Terraform" > /var/www.html/index.html
-    service httpd start
-    chkconfig httpd on
+#!/bin/bash
+yum -y update
+yum -y install httpd
+MYIP=`curl http://169.254.169.254/lastest/meta-data/local-ipv4`
+echo "<h2>Webserver with PrivateIP: $MYIP</h2><br>Build by Terraform" > /var/www.html/index.html
+service httpd start
+chkconfig httpd on
 EOF
     tags = {
         Name = "Webserver Build by Terraform"
         Owner = "Hsu"   
     }
 }
+
 resource "aws_security_group" "web" {
     name = "Webserver-ORG"
     description = "Security Group for my webserver"
