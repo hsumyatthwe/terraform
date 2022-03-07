27cec12bd6a522b9b2f30fb9ab19d290b6e2b3e6
hsumyatthwe <hsumyatthwe@gtmh-telecom.com>
2022-03-07 16:24:37 +0630

    commit code

--- basicweblab/main.tf
+++ basicweblab/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
     ami = "ami-0b9f27b05e1de14e9" // Amaxon Linux2
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.web.id]
-    user_data = << EOF 
+    user_data = <<EOF 
 #!/bin/bash
 yum -y update
 yum -y install httpd
